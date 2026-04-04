a1c57c70dd3f0c7eb350af3001c0ea1585d7db1f
Emma Esser Díaz <emma@MacBook-Pro-de-Emma-2.local>
2026-04-04 18:25:36 +0200

    estructura basica

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const resultsUl = document.querySelector(".js_results");
+
+resultsUl.innerHTML = `
+<li class="series-card">
+     <img class="series-img" src="./image/GOT.jpg" alt="Serie-nova">
+    <p class="series-title">Game of Thrones</p>
+ </li>`;
